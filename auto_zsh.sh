a815de90e5088793f623b0ad72ad5a2bbb56aaee
Chibraax <56515202+Chibraax@users.noreply.github.com>
2024-09-22 16:16:05 +0200

    Update auto_zsh.sh

--- auto_zsh.sh
+++ auto_zsh.sh
@@ -217,11 +217,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ;
 mv zsh* ~/.oh-my-zsh/plugins
 
 # Copy personnal theme into dir
-mv personnal.zsh-theme ~/.oh-my-zsh/themes/;
+mv chibraax.zsh-theme ~/.oh-my-zsh/themes/;
 
 # Write plug-in into .zshrc and change theme
 sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)/g" .zshrc ;
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="duellj"/g' .zshrc ;
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="chibraax"/g' .zshrc ;
 
 # Config .zshrc
 echo "fastfetch" >> .zshrc
